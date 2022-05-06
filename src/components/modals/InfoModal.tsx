faf79b19fa2e5cc9a190bf073a3b41e4147d116c
tropicbliss <tropicbliss@protonmail.com>
2022-05-06 17:14:02 +0800

    Update infomodal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,4 +1,3 @@
-import { Cell } from '../grid/Cell'
 import { BaseModal } from './BaseModal'
 
 type Props = {
@@ -19,47 +18,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         only Brandino "Coffee Cow" Ewing likes it, perchance.
       </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="W"
-          status="correct"
-        />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        Normal Wordle rules apply.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
